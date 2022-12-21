fc07e692a7dff13e9f18863314953bbf404153e3
Nicoleta Bulzan <nicoletabulzan94@gmail.com>
2022-12-21 18:10:06 +0200

    Tema Lab17

--- src/test/java/Cart.java
+++ src/test/java/Cart.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Cart {
+    private WebDriver driver;
+
+    @Before
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        driver.findElement(By.cssSelector(".skip-link.skip-account .label")).click();
+        driver.findElement(By.cssSelector("#header-account .links li:nth-child(6) a")).click();
+        driver.findElement(By.id("email")).sendKeys("test@yopmail.com");
+        driver.findElement(By.id("pass")).sendKeys("test123");
+        driver.findElement(By.id("send2")).click();
+
+    }
+
 }
